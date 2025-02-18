96cf3d2c6cd13d56571d2896f471c15b31a43217
giorgizena123 <zenaishvili621@gmail.com>
2025-02-19 00:11:44 +0400

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
 
-public class Human {
+class Human  implements Serializable {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Human(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public void walk() {
+        System.out.println(firstName + " is walking.");
+    }
+
+    public void hangOut(Human friend) {
+        System.out.println(this.firstName + " is hanging out with " + friend.firstName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
